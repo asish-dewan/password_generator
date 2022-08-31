9cec971aecbd283d781d3df155bb40fed62dcdd8
asish-dewan <104346122+asish-dewan@users.noreply.github.com>
2022-08-31 22:27:43 +1000

    Add character and password length variable + pseudocode steps to generate password

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var char = "0123456789Abcdefghijklmnopqrstuvwxyz!#$%&!#$%&'()*+,-.:;<=>?@]^_`{|}~";
+var passwordlength = 128;
 
 // Write password to the #password input
 function writePassword() {
@@ -10,5 +12,25 @@ function writePassword() {
 
 }
 
+// Define generatePassword
+
+function generatePassword () {
+
+// Prompt for the length of the password
+  prompt ('Enter your password length')
+
+// Limit min length 8 and max length 128 
+
+// Lowercase prompt
+
+// Uppercase prompt
+
+// Numeric prompt
+
+// Special characters prompt (This can be and/or option)
+
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
