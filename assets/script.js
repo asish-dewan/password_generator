4dcfcc48bcca60f4c41f5eb6900fcb675873b858
asish-dewan <104346122+asish-dewan@users.noreply.github.com>
2022-09-01 23:30:23 +1000

    Format code

--- assets/script.js
+++ assets/script.js
@@ -4,59 +4,69 @@ var uCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var lCase = 'abcdefghijklmnopqrstuvwyz';
 var numeric = '0123456789';
 var specialchar = " !#$%&!#$%&'()*+,-.:;<=>?@]^_`{|}~ ";
-var validate = (uCase && lCase && numeric && specialchar);
+//var validate = (uCase && lCase && numeric && specialchar);
 
 // Define generatePassword
 
-function generatePassword () {
+function generatePassword() {
   var randomPass = "";
 
-// Choose length of password
-let passwordlength = window.prompt ("Choose the length of your password");
+  // Choose length of password
+  let passwordlength = window.prompt("Choose the length of your password");
 
-// Add min and max conditions
-if (passwordlength < 8) {
-  alert ("Password is too short. Mininum number = 8");
-}
-else if (passwordlength > 128) {
-  alert ("Password is too long. Max number = 128");
-}
-// Confirm Uppercase
-else {
-  let uCase = window.confirm ("Do you want to include 'UPPERCASE' characters?");
-  if (uCase == true) {
-    randomPass += uCase
+  // Add min and max conditions
+  if (passwordlength < 8) {
+    alert("Password is too short. Mininum number = 8");
+  } else if (passwordlength > 128) {
+    alert("Password is too long. Max number = 128");
   }
-  // Confirm Lowercase
-  let lCase = window.confirm ("Do you want to include 'lowercase' characters?");
-  if (lCase == true) {
-    randomPass += lCase
-  }
-  // Confirm numeric and/or special characters
-  let numeric = window.confirm ("Do you want to include 'Numeric' characters?");
-  if (numeric == true) {
-    randomPass += numeric
-  }
-  let specialchar = window.confirm ("Do you want to include 'Special' characters?");
-  if (specialchar == true) {
-    randomPass += specialchar
-  }
+  // Confirm Uppercase
+  else {
+    let uCase = window.confirm(
+      "Do you want to include 'UPPERCASE' characters?"
+    );
+    if (uCase == true) {
+      randomPass += uCase;
+    }
+    // Confirm Lowercase
+    let lCase = window.confirm(
+      "Do you want to include 'lowercase' characters?"
+    );
+    if (lCase == true) {
+      randomPass += lCase;
+    }
+    // Confirm numeric and/or special characters
+    let numeric = window.confirm(
+      "Do you want to include 'Numeric' characters?"
+    );
+    if (numeric == true) {
+      randomPass += numeric;
+    }
+    let specialchar = window.confirm(
+      "Do you want to include 'Special' characters?"
+    );
+    if (specialchar == true) {
+      randomPass += specialchar;
+    }
 
-  // Validate at least one character selected
-  let validate = window.confirm ("Have you selected at least ONE character type?")
-  if (validate = !true) {
-    alert ("Please select at least ONE character type");
-  } 
-
-}
+    // Validate at least one character selected
+    let validate = window.confirm(
+      "Have you selected at least ONE character type?"
+    );
+    if ((validate = !true)) {
+      alert("Please select at least ONE character type");
+    }
+  
+  }
 
   let finalpassword = "";
   for (let i = 0; i < passwordlength; i++) {
-    finalpassword += randomPass.charAt(Math.floor(Math.random() * randomPass.length))
-    } 
-    return finalpassword
-
+    finalpassword += randomPass.charAt(
+      Math.floor(Math.random() * randomPass.length)
+    );
   }
+  return finalpassword;
+}
 
 // Run write password script
 
