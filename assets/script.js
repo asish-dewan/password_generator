e25cff1a40d0c3593d40a92c955c6986be964056
asish-dewan <104346122+asish-dewan@users.noreply.github.com>
2022-09-01 22:08:54 +1000

    Redefine generatePassword function and rework logic pseudo code

--- assets/script.js
+++ assets/script.js
@@ -1,44 +1,37 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var char = ["0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&!#$%&'()*+,-.:;<=>?@]^_`{|}~"];
-var passwordlength = (8 <= 128);
-
 
 // Define generatePassword
 
 function generatePassword () {
+  var chars = ["0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&!#$%&'()*+,-.:;<=>?@]^_`{|}~"];
 
-  // Prompt for the length of the password
-    var passwordlength = prompt ('Enter your password length');
-  
-  // Limit min length 8 and max length 128 
-  if (passwordlength < 8 ) {
-      alert ("Password does not meet the min. length requirement. Please try again. ");
-    } 
-    
-  else if (passwordlength > 128) {
-    alert ("Password is too long. Please enter try again. ");
-    }
-  
-  // Lowercase, Uppercase and numeric prompts
-    else {
-      window.alert ("Click OK to include ' lowercase ' characters");
-      window.alert ("Click OK to include ' UPPERCASE ' characters");
-      window.alert ("Click OK to include ' numeric ' characters");
-    }
-  
-  // Validate input
-  var randomPass = '';
-  for (var i = 0; i === passwordlength; i++){
-    var index = Math.floor(Math.random() * char.length)
-    index += char.substring(index, index + 1)
-    } 
-    
-    console.log (randomPass)
-  // Display password 
-  }
+  var passwordlength = 128;
+  var password = "";
 
-// Write password to the #password input
+  for (var i = 0; i <= passwordlength; i++)
+  {
+    var index = Math.floor(Math.random() * chars.length)
+    password += chars[index];
+    } 
+
+    console.log ("button clicked")
+
+}
+// Choose length of password
+
+// Add min and max conditions
+
+// Confirm Uppercase
+
+// Confirm Lowercase
+
+// Confirm numeric and/or special characters
+
+// Validate at least one character selected
+
+// Run write password script
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
